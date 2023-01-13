1616bd8ec2ae920730631ec79d49a783d519d11f
Chun Lau <dlaucodes@gmail.com>
2023-01-13 15:54:58 -0500

    remove click handler

--- src/index.js
+++ src/index.js
@@ -53,19 +53,22 @@ document.addEventListener('DOMContentLoaded', ()=> {
         // audio1.addEventListener('click, function(')
         // audio1.play();
         audio1.muted();
-    
-    });
-
-
-    
-    file.addEventListener('click', ()=>{
-        const files = this.files;
-        const audio1 = document.getElementById('audio1')
-        // audio1.src = URL.createObjectURL(files[0]);
         audio1.load();
         audio1.resume();
         audio1.play();
+    
     });
+
+
+    
+    // file.addEventListener('click', ()=>{
+    //     const files = this.files;
+    //     const audio1 = document.getElementById('audio1')
+    //     // audio1.src = URL.createObjectURL(files[0]);
+    //     audio1.load();
+    //     audio1.resume();
+    //     audio1.play();
+    // });
 
     const audioContext = new AudioContext()
     // audio1.play();
